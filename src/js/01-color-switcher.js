dca0106465e992cd98477afa6b0836ee8aa72f47
Andrii Berezhko <Berezhko7777@ukr.net>
2022-05-07 15:44:01 +0300

    1 task
    1 task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
 
+const refs = {
+    body: document.querySelector('body'),
+    startButton: document.querySelector('[data-start]'),
+    stopButton: document.querySelector('[data-stop]')
+};
+
+
+refs.startButton.addEventListener('click', onStartClick);
+refs.stopButton.addEventListener('click', onStopClick);
+refs.stopButton.setAttribute('disabled', 'true');
+let timerId = null;
+
+function onStartClick() {
+    
+    refs.startButton.setAttribute('disabled', 'true');
+    refs.stopButton.removeAttribute('disabled');
+    timerId = setInterval(() => {
+        refs.body.style.background = getRandomHexColor();
+    }, 1000);
+    
+};
+
+function onStopClick() {
+    refs.startButton.removeAttribute('disabled');
+    refs.stopButton.setAttribute('disabled', 'true');
+    clearInterval(timerId);
+};
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
